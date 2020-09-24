203a13edc8a9d34929da6c957ea4009f912f7679
Zhong-Shan-Hui <71785870+Zhong-Shan-Hui@users.noreply.github.com>
2020-09-24 09:38:23 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+session_destroy();
+header('Location: index.php');
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zhong Shan Hui Logout</title>
+</head>
+<body>
+
+</body>
+</html>
